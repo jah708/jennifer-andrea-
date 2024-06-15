a1e0ee6e848455c7d5bf8c51698a94a349d5ded2
jah708 <andrea.16.henao@hotmail.com>
2024-06-14 20:52:53 -0500

    entrega

---
+++ scripts.js
@@ -0,0 +1,12 @@
+console.log("hola")
+let num1=5
+let num2=7
+
+const sumar= num1+num2
+console.log("resultado de la suma es" , sumar) 
+const restar= num1-num2
+console.log("resultado de la resta es" , restar)
+const multiplicar= num1*num2
+console.log ("resultado de la multiplicacion es" , multiplicar)
+const dividir= num1/num2
+console.log ("resultado de la division" , dividir)
